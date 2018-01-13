49ae663a02b47815682c5667ae5756fc5ad09aff
Bryan Veloso <bryan@avalonstar.com>
2018-01-13 15:00:04 -0800

    Add Tungsten.

--- src/helpers/foundation.js
+++ src/helpers/foundation.js
@@ -1,6 +1,7 @@
 const foundation = {
   gotham: "'Gotham SSm A', 'Gotham SSm B'",
-  ideal: "'Ideal Sans SSm A', 'Ideal Sans SSm B'"
+  ideal: "'Ideal Sans SSm A', 'Ideal Sans SSm B'",
+  tungsten: "'Tungsten A', 'Tungsten B'"
 };
 
 export default foundation;

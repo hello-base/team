50007bf212ac3410811759caf3779778c996b078
Bryan Veloso <bryan@avalonstar.com>
2018-01-29 22:30:49 -0800

    First pass at Caster styling. More work to be done.

--- src/components/Episode/Casters.js
+++ src/components/Episode/Casters.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { List } from 'immutable';
-import moment from 'moment';
-import uniqid from 'uniqid';
 
 import styled from 'styled-components';
 
 const propTypes = {
   casters: PropTypes.instanceOf(List).isRequired
 };
+
+const Display = props => (
+  <Wrapper>
+    {props.casters.map(caster => (
+      <Caster key={caster.get('username')}>
+        <Name>{caster.get('nickname')}</Name>
+        <Handle>{caster.get('username')}</Handle>
+        {caster.get('biography')}
+        <Oshimen>
+          <Header>Oshimen</Header>
+          <strong>{caster.get('kamioshi')}</strong>,{' '}
+          {caster.get('oshi_overall')}
+        </Oshimen>
+        <Oshimen>
+          <Header>Current H!P Loves</Header>
+          {caster.get('oshi_current')}
+        </Oshimen>
+      </Caster>
+    ))}
+  </Wrapper>
+);
+
+Display.propTypes = propTypes;
 
 const Wrapper = styled.div`
   grid-column: span 3;
@@ -22,32 +43,44 @@ const Wrapper = styled.div`
   color: #fff;
 `;
 
+const Caster = styled.div`
+  padding-top: 10px;
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    width: 30px;
+    height: 3px;
+
+    background: #2db0ea;
+    border-radius: 3px;
+  }
+`;
+
 const Name = styled.div`
   flex: 1;
   font-family: ${props => props.theme.tungsten};
   font-size: 36px;
 `;
 
-const Handle = styled.div``;
+const Handle = styled.div`
+  display: none;
+`;
 
-const Oshi = styled.div``;
+const Oshimen = styled.div`
+  padding: 6px 0;
+  font-family: ${props => props.theme.ideal};
+  font-style: italic;
+`;
 
-const Display = props => (
-  <Wrapper>
-    {props.casters.map(caster => (
-      <div key={caster.get('username')}>
-        <Name>{caster.get('nickname')}</Name>
-        <Handle>{caster.get('username')}</Handle>
-        <Oshi>
-          <strong>{caster.get('kamioshi')}</strong>,{' '}
-          {caster.get('oshi_overall')}
-          Current H!P Loves {caster.get('oshi_current')}
-        </Oshi>
-      </div>
-    ))}
-  </Wrapper>
-);
-
-Display.propTypes = propTypes;
+const Header = styled.div`
+  color: #8cd3f3;
+  font-family: ${props => props.theme.gotham};
+  font-size: 13px;
+  font-style: normal;
+  text-transform: uppercase;
+`;
 
 export default Display;
